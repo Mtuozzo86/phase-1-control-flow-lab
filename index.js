20c02e07a33132a30593811ef3b071ebf6e2a508
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-06-29 21:02:04 -0400

    test a push

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(footage){
+    if(footage <= 400){
+    return 'This one is on me!'
+  } else if (footage > 2000 && footage < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (footage > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+} 
+
+
+// function switchOnCharmFromTip(tip){
+//   if (tip === 'generous') {
+//     return 'Thank you so much.';
+//   } else if (tip === 'not as generous') {
+//     return 'Thank you.';
+//   } else if (tip === 'thanks for everything') {
+//     return 'Bye.'
+//   } 
+
+// }
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    case 'thanks for everything':
+      return 'Bye.';
+}
 }
